6ebeb74a638244eceefd5e6aa62961fa9bb7a061
jknyght9 <jstauffer@infocyte.com>
2016-08-08 01:25:11 -0500

    modified setup scripts

---
+++ cfc-chromebook-setup1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget https://goo.gl/fd3zc
+sudo sh -e ~/Downloads/crouton -r trusty -t gnome,unity
+sudo startgnome
--- cfc-guest-setup2.sh
+++ cfc-guest-setup2.sh
@@ -3,7 +3,7 @@
 sudo apt-get update && apt-get upgrade
 sudo apt-get install libfuse2 mapivi keepassx
 cd /tmp
-wget -O veracrypt.tar.bz2 https://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=veracrypt&DownloadId=1537179&FileTime=130999181365600000&Build=21031
+wget -O veracrypt.tar.bz2 "https://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=veracrypt&DownloadId=1537179&FileTime=130999181365600000&Build=21031"
 tar -jvxf veracrypt.tar.bz2
 cd veracrypt
 ./veracrypt-1.17-setup-gui-x64
