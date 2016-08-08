da067786babf45a5f9a9e20df6cab4139d38b26c
jknyght9 <jstauffer@infocyte.com>
2016-08-08 00:34:31 -0500

    modified veracrypt download link

--- cfc-laptop-setup.sh
+++ cfc-laptop-setup.sh
@@ -3,7 +3,7 @@
 sudo apt-get update && apt-get upgrade
 sudo apt-get install libfuse2 mapivi keepassx
 cd /tmp
-wget https://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=veracrypt&DownloadId=1537179&FileTime=130999181365600000&Build=21031
+wget https://sourceforge.net/projects/veracrypt/files/latest/download
 tar -jvxf veracrypt-1.17-setup.tar.bz2
 ./veracrypt-1.17-setup-gui-x64
 cd /home/pete/Desktop/
