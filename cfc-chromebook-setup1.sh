513cc5dbcb846b76731bf2824da13f9ec05ecf39
jknyght9 <jstauffer@infocyte.com>
2016-08-09 20:44:14 -0500

    updated chromebook script

--- cfc-chromebook-setup1.sh
+++ cfc-chromebook-setup1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-wget https://goo.gl/fd3zc
+wget -O crouton https://goo.gl/fd3zc
 wget https://raw.githubusercontent.com/jknyght9/CyberForensicsChallenge/master/cfc-guest-setup2.sh
 chown 777 ~/Downloads/cfc-guest-setup2.sh
-sudo sh -e ~/Downloads/crouton -r trusty -t gnome,unity
+sudo sh -e crouton -r trusty -t gnome,unity
 sudo startgnome
